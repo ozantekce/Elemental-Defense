d59833bc8a5339ae510efbcde1b45697f35630c9
ozantekce <ozantekce@gmail.com>
2023-03-23 07:28:35 +0300

    feat : level design, ui

---
+++ Assets/TerrainSetter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSetter : MonoBehaviour
+{
+    void Start()
+    {
+        Terrain.activeTerrain.detailObjectDistance = 750;
+    }
+}
